4369f66b72e684fbd9bfe23f2279a3694e94f1e9
Debashish M <10178321+GridPen@users.noreply.github.com>
2022-02-14 22:47:00 +0600

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Debashish Mondal on 2/14/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
