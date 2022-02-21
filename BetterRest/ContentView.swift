515f1b29975fc44f311dc1159c27f5e5f557b696
Debashish M <10178321+GridPen@users.noreply.github.com>
2022-02-22 01:10:44 +0600

    Adding Core ML

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -5,19 +5,60 @@
 //  Created by Debashish Mondal on 2/14/22.
 //
 
+
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
+    
+    
+    
     var body: some View {
-        ZStack{
-            LinearGradient(gradient: Gradient(colors: [.blue, .indigo]), startPoint: .bottom, endPoint: .top)
-                .ignoresSafeArea()
-            
-            Text("Hello")
-                .font(.system(size: 20))
+        NavigationView{
+            ZStack{
+                LinearGradient(gradient: Gradient(colors: [.blue, .white, .mint]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                    .ignoresSafeArea()
+                
+                VStack{
+                    Text("When you want to wake up?")
+                        .font(.headline)
+                    DatePicker("Please enter a time", selection: $wakeUp, in: Date.now...,  displayedComponents: [.hourAndMinute])
+                        .labelsHidden()
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                    Text("Your daily coffe intake")
+                        .font(.headline)
+                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value:$coffeeAmount,  in: 1...20)
+                    
+                }
+            }
+            .navigationTitle("BetterRest")
+            .toolbar{
+                Button("calculate", action: calculateBedTime)
+            }
         }
 
 
+        
+    }
+    func calculateBedTime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            let sleepTime = wakeUp - prediction.actualSleep
+        } catch{
+            
+        }
     }
 }
 
